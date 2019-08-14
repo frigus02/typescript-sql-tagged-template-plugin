24ea21ad22bd417f0353211def6cfa67a3d1cd8c
Jan Kuehle <jan.kuehle@red-badger.com>
2019-08-14 15:18:47 +0100

    Properly parse schema json

---
+++ src/schema/index.test.ts
@@ -0,0 +1,25 @@
+import { parseSchema } from "./index";
+
+describe("parseSchema", () => {
+	it("returns parsed database schema", () => {
+		expect(parseSchema(`{"public": {"users": {"id": "number"}}}`)).toEqual({
+			public: {
+				users: {
+					id: "number"
+				}
+			}
+		});
+	});
+
+	it("throws if schema is invalid JSON", () => {
+		expect(() => parseSchema("{error,}")).toThrow(
+			"Unexpected token e in JSON at position 1"
+		);
+	});
+
+	it("throws if schema is not a valid DatabaseSchema", () => {
+		expect(() => parseSchema(`{"users": {"id": "number"}}`)).toThrow(
+			"Schema does not conform to database schema type"
+		);
+	});
+});
--- src/schema/index.ts
+++ src/schema/index.ts
@@ -12,6 +12,35 @@ export interface DatabaseSchema {
 	[schemaName: string]: SchemaDefinition | undefined;
 }
 
+const isColumnDefinition = (obj: unknown): obj is ColumnDefinition =>
+	typeof obj === "string";
+
+const isTableDefinition = (obj: unknown): obj is TableDefinition =>
+	typeof obj === "object" &&
+	obj !== null &&
+	(Object.keys(obj) as Array<keyof typeof obj>).every(key =>
+		isColumnDefinition(obj[key])
+	);
+
+const isSchemaDefinition = (obj: unknown): obj is SchemaDefinition =>
+	typeof obj === "object" &&
+	obj !== null &&
+	(Object.keys(obj) as Array<keyof typeof obj>).every(key =>
+		isTableDefinition(obj[key])
+	);
+
+const isDatabaseSchema = (obj: unknown): obj is DatabaseSchema =>
+	typeof obj === "object" &&
+	obj !== null &&
+	(Object.keys(obj) as Array<keyof typeof obj>).every(key =>
+		isSchemaDefinition(obj[key])
+	);
+
 export const parseSchema = (json: string): DatabaseSchema => {
-	return JSON.parse(json);
+	const obj = JSON.parse(json) as unknown;
+	if (!isDatabaseSchema(obj)) {
+		throw new Error("Schema does not conform to database schema type");
+	}
+
+	return obj;
 };
